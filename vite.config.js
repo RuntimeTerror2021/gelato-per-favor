fd893e7c7887b1ab060d2f41e74cb67c7037bfb0
Aaryan Soni <aaryansoni577@gmail.com>
2025-07-24 14:33:48 -0500

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                privacy: resolve(__dirname, 'privacy.html')
+                // login: resolve(__dirname, "auth/login.html"),
+                // signup: resolve(__dirname, "auth/signup.html"),
+                // about: resolve(__dirname, "about.html"),
+                // explore: resolve(__dirname, "explore.html"),
+                // trips: resolve(__dirname, "trips.html"),
+                // home: resolve(__dirname, "home.html"),
+                // saved: resolve(__dirname, "saved.html"),
+                // profile: resolve(__dirname, "profile.html"),
+            },
+        },
+    }
+
+})
